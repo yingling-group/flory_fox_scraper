8ffa832dc9a0d036175e57354cc25b47fc4aeb05
James Peerless <jspeerle@ncsu.edu>
2018-03-02 15:28:33 -0500

    add PoLyInfo login info

--- scrape_polyinfo.py
+++ scrape_polyinfo.py
@@ -10,21 +10,64 @@ Author: James S. Peerless
 For internal evaluation by Citrine Informatics only.
 '''
 
-##############################################################################
-#        Initialization                                                      #
-##############################################################################
+###############################################################################
+#        Initialization                                                       #
+###############################################################################
 
 import numpy as np
 import time
+import requests
+from lxml import html
 
 # Start the clock
 t_start = time.time()
 update = 1
 total_points = 0
 
-##############################################################################
-#        Denoument                                                           #
-##############################################################################
+###############################################################################
+#        PolyInfo Login                                                       #
+###############################################################################
+
+# My PolyInfo Login Info
+payload = {
+	"IDToken1" : "jspeerle@ncsu.edu",
+	"IDToken2" : "0rr!S1*SAa1b",
+	"IDToken0" : "",
+	"goto" : "aHR0cDovL3BvbHltZXIubmltcy5nby5qcDo4MC9Qb0x5SW5mby8=",
+	"SunQueryParamsString" : "",
+	"encoded" : "true",
+	"gx_charset" : "UTF-8"
+}
+
+session_requests = requests.session()
+login_url = "https://login-matnavi.nims.go.jp/sso/UI/Login?goto="\
+            + "http%3A%2F%2Fpolymer.nims.go.jp%3A80%2FPoLyInfo%2F"
+result = session_requests.get(login_url)
+tree = html.fromstring(result.text)
+
+result = session_requests.post(
+	login_url,
+	data = payload,
+	headers = dict(referer=login_url)
+)
+
+# Test login by accessing search page
+url = 'http://polymer.nims.go.jp/PoLyInfo/cgi-bin/p-search.cgi'
+result = session_requests.get(
+	url,
+	headers = dict(referer = url)
+)
+
+print result
+
+tree = html.fromstring(result.content)
+
+if result.ok == False:
+	print "PoLyInfo Login Error. Check username/password!"
+
+###############################################################################
+#        Denoument                                                            #
+###############################################################################
 
 total_time = time.time() - t_start
 print 'Success!'
