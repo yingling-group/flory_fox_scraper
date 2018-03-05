9c8f49e9da5597cad75b6ed4bac30b2c17cf44db
James Peerless <jspeerle@ncsu.edu>
2018-03-05 14:26:15 -0500

    Successful PolyInfo login

--- scrape_polyinfo.py
+++ scrape_polyinfo.py
@@ -17,7 +17,6 @@ For internal evaluation by Citrine Informatics only.
 import numpy as np
 import time
 import requests
-from lxml import html
 
 # Start the clock
 t_start = time.time()
@@ -31,39 +30,21 @@ total_points = 0
 # My PolyInfo Login Info
 payload = {
 	"IDToken1" : "jspeerle@ncsu.edu",
-	"IDToken2" : "0rr!S1*SAa1b",
+	"IDToken2" : "0rr!S1*SAs1b",
 	"IDToken0" : "",
-	"goto" : "aHR0cDovL3BvbHltZXIubmltcy5nby5qcDo4MC9Qb0x5SW5mby8=",
-	"SunQueryParamsString" : "",
-	"encoded" : "true",
-	"gx_charset" : "UTF-8"
 }
 
 session_requests = requests.session()
 login_url = "https://login-matnavi.nims.go.jp/sso/UI/Login?goto="\
-            + "http%3A%2F%2Fpolymer.nims.go.jp%3A80%2FPoLyInfo%2F"
-result = session_requests.get(login_url)
-tree = html.fromstring(result.text)
+            + "http%3A%2F%2Fpolymer.nims.go.jp%3A80%2FPoLyInfo%2Fcgi-bin%2Fp-search.cgi"
 
-result = session_requests.post(
+result = requests.post(
 	login_url,
 	data = payload,
-	headers = dict(referer=login_url)
 )
 
-# Test login by accessing search page
-url = 'http://polymer.nims.go.jp/PoLyInfo/cgi-bin/p-search.cgi'
-result = session_requests.get(
-	url,
-	headers = dict(referer = url)
-)
-
-print result
-
-tree = html.fromstring(result.content)
-
-if result.ok == False:
-	print "PoLyInfo Login Error. Check username/password!"
+if 'Authentication failed.' in result.content:
+	print "ERROR: PoLyInfo login unsuccessful. Check username and/or password!"
 
 ###############################################################################
 #        Denoument                                                            #
