0066bece3538992c6adbf9892fc045a7d09e8323
James Peerless <jspeerle@ncsu.edu>
2018-03-06 09:45:29 -0500

    Attempt to reach Easy Browse Page

--- scrape_polyinfo.py
+++ scrape_polyinfo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 '''
-PolyInfo Database Scraper - scrape_polyinfo.py
+PoLyInfo Database Scraper - scrape_polyinfo.py
 
 Author: James S. Peerless
         Yingling Group
@@ -14,9 +14,10 @@ For internal evaluation by Citrine Informatics only.
 #        Initialization                                                       #
 ###############################################################################
 
-import numpy as np
 import time
 import requests
+from bs4 import BeautifulSoup
+import urllib2
 
 # Start the clock
 t_start = time.time()
@@ -24,27 +25,36 @@ update = 1
 total_points = 0
 
 ###############################################################################
-#        PolyInfo Login                                                       #
+#        PoLyInfo Login                                                       #
 ###############################################################################
 
-# My PolyInfo Login Info
-payload = {
+# My PoLyInfo Login Info
+login_info = {
 	"IDToken1" : "jspeerle@ncsu.edu",
 	"IDToken2" : "0rr!S1*SAs1b",
 	"IDToken0" : "",
 }
 
-session_requests = requests.session()
+# Login to PoLyInfo search page
 login_url = "https://login-matnavi.nims.go.jp/sso/UI/Login?goto="\
-            + "http%3A%2F%2Fpolymer.nims.go.jp%3A80%2FPoLyInfo%2Fcgi-bin%2Fp-search.cgi"
-
-result = requests.post(
+            + "http%3A%2F%2Fpolymer.nims.go.jp%3A80%2FPoLyInfo%2Fcgi-bin%2Fp"\
+            +"-search.cgi"
+login_result = requests.post(
 	login_url,
-	data = payload,
+	data = login_info,
 )
-
-if 'Authentication failed.' in result.content:
+if 'Authentication failed.' in login_result.content:
 	print "ERROR: PoLyInfo login unsuccessful. Check username and/or password!"
+
+###############################################################################
+#        Data Import                                                          #
+###############################################################################
+
+# Get polymer classes
+ez_browse_url = "http://polymer.nims.go.jp/PoLyInfo/cgi-bin/p-easy-ptable.cgi"
+ez_browse_html = urllib2.urlopen(ez_browse_url)
+ez_browse = BeautifulSoup(ez_browse_html,"lxml")
+
 
 ###############################################################################
 #        Denoument                                                            #
